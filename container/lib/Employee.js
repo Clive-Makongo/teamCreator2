2dad3908da9e166d889facc7a776a545f1629725
Clive Makongo <clivemakongo@gmail.com>
2024-01-21 01:54:55 +0000

    created file structure

---
+++ container/lib/Employee.js
@@ -0,0 +1,25 @@
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        const name = this.name;
+        const id = this.id;
+        const email = this.email;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return 
+
+    }
+}
